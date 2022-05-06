88443c94afaa4d0d6cdbe2851ef5a6ad276a9863
binhnhu1409 <binhnhu1409@gmail.com>
2022-05-06 15:43:01 +0300

    initial look of header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,23 @@
 import React, { useContext } from 'react'
 
 import { BsFillBookmarkHeartFill } from 'react-icons/bs'
+
+import './header.scss'
 import { ThemeContext } from '../../context/theme'
 
 const Header = () => {
   const { theme, switchTheme } = useContext(ThemeContext)
   console.log('THEME:', theme)
+
   return (
-    <header style={{ backgroundColor: theme.foreground }}>
+    <header style={{ backgroundColor: theme.foreground }} className="head">
       <nav className="navbar">
         <ul className="navbar__menu">
           <li className="navbar__item">
             <h1 className="navbar__logo">Countries</h1>
           </li>
-          <li>
-            <BsFillBookmarkHeartFill />
+          <li className="navbar__item">
+            <BsFillBookmarkHeartFill className="navbar__icon" />
           </li>
         </ul>
       </nav>
